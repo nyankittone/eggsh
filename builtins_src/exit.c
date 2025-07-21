5422edc333e60c92e02d2e80bdbd99dd0260edde
nyankittone <farrisluke8@gmail.com>
2025-07-21 02:03:49 -0400

    Implement `exit` command

--- builtins_src/exit.c
+++ builtins_src/exit.c
@@ -1,8 +1,22 @@
+#include <rusttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <builtin_commands.h>
 
+// I hope this conforms to POSIX. Didn't look at the spec for exit *too* deeply on this one...
 mBuiltin(commands_exit) {
-    puts(argv[0]);
+    if(!argv[1]) exit(0);
+
+    char *hit_end = NULL;
+    long exit_code = strtol(argv[1], &hit_end, 10);
+    if(errno || *(hit_end + strspn(hit_end, " \t\n"))) {
+        fputs("Invalid exit code passed! (should be a non-negative integer)\n", stderr);
+        return 1;
+    } 
+
+    exit(exit_code);
     return 0;
 }
 
--- todo.md
+++ todo.md
@@ -1,6 +1,6 @@
 # TODO list
 * Implement the cd, echo, and exit commands
-* Create ways to unit test builtin commnands
+* Create ways to unit test builtin commands
 * Implement reading from $PATH (almost done!)
 * Add support for reading commands from a file
 * Add parsing for single and double quotes, backslashes, and semicolons
